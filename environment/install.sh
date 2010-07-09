ef96b176aabfe89f1828f1e4c1fe656b6ab8183c
Remi Broemeling <remi@broemeling.org>
2010-07-09 02:38:11 +0000

    Add an installation script that can be used to install my personal environment in an arbitrary location (local or remote).

---
+++ environment/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+#
+# A simple script to install the environment files on a given host (or in a given location).
+#
+
+if [ -z "${1}" ]; then
+	echo "Usage: ${0} DEST" >&2
+	exit -1
+fi
+
+if [[ "${1}" =~ ":" ]]; then
+	HOST="${1%%:*}"
+else
+	HOST=""
+fi
+LOCATION="${1#*:}"
+
+if [ -n "${HOST}" ]; then
+	rsync -rlptv --exclude=".svn" --rsh="ssh" . "${HOST}:${LOCATION}/.environment-install"
+	echo "
+		cd '${LOCATION}/.environment-install'
+		./install.sh '${LOCATION}'
+		cd '${LOCATION}'
+		rm -rv '${LOCATION}/.environment-install'
+	" | ssh "${HOST}" bash
+else
+	for F in .bash_logout .bashrc .emacs .profile .vimrc; do
+		install --backup --mode=0644 --preserve-timestamps --verbose "${F}" "${LOCATION}/"
+	done
+fi
