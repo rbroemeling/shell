ed9868a419be95b0928761ab100f26db8b99a853
Remi Broemeling <remi@broemeling.org>
2011-05-21 23:23:17 -0600

    Whitespace changes: tabs to spaces.  No functional changes.

--- home/install.sh
+++ home/install.sh
@@ -4,30 +4,30 @@
 #
 
 if [ -z "${1}" ]; then
-	echo "Usage: ${0} DEST" >&2
-	exit -1
+  echo "Usage: ${0} DEST" >&2
+  exit -1
 fi
 
 if [[ "${1}" =~ ":" ]]; then
-	HOST="${1%%:*}"
+  HOST="${1%%:*}"
 else
-	HOST=""
+  HOST=""
 fi
 LOCATION="${1#*:}"
 
 if [ -n "${HOST}" ]; then
-	rsync -rlptv --exclude=".svn" --rsh="ssh" . "${HOST}:${LOCATION}/.environment-install"
-	echo "
-		cd '${LOCATION}/.environment-install'
-		./install.sh '${LOCATION}'
-		cd '${LOCATION}'
-		rm -rv '${LOCATION}/.environment-install'
-	" | ssh "${HOST}" bash
+  rsync -rlptv --exclude=".svn" --rsh="ssh" . "${HOST}:${LOCATION}/.environment-install"
+  echo "
+    cd '${LOCATION}/.environment-install'
+    ./install.sh '${LOCATION}'
+    cd '${LOCATION}'
+    rm -rv '${LOCATION}/.environment-install'
+  " | ssh "${HOST}" bash
 else
   find . -type f | while read F; do
     F="${F:2}"
-		if ! diff -q -N "${F}" "${LOCATION}/${F}" >/dev/null 2>&1; then
-			install -D --backup --mode=0644 --preserve-timestamps --verbose "${F}" "${LOCATION}/${F}"
-		fi
+    if ! diff -q -N "${F}" "${LOCATION}/${F}" >/dev/null 2>&1; then
+      install -D --backup --mode=0644 --preserve-timestamps --verbose "${F}" "${LOCATION}/${F}"
+    fi
   done
 fi
