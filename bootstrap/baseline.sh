8952b0013df54bb75f7d6190e83f98baa6e47e9e
Remi Broemeling <remi@broemeling.org>
2013-10-14 08:41:33 -0600

    Update squeeze -> wheezy.

--- bootstrap/baseline.sh
+++ bootstrap/baseline.sh
@@ -31,14 +31,14 @@ read UNPRIVILEGED_USER
 # Configure APT.
 #
 cat >/etc/apt/sources.list <<EOF
-deb http://ftp.ca.debian.org/debian/ squeeze main non-free contrib
-deb-src http://ftp.ca.debian.org/debian/ squeeze main non-free contrib
+deb http://ftp.ca.debian.org/debian/ wheezy main non-free contrib
+deb-src http://ftp.ca.debian.org/debian/ wheezy main non-free contrib
 
-deb http://security.debian.org/ squeeze/updates main non-free contrib
-deb-src http://security.debian.org/ squeeze/updates main non-free contrib
+deb http://security.debian.org/ wheezy/updates main non-free contrib
+deb-src http://security.debian.org/ wheezy/updates main non-free contrib
 
-deb http://ftp.ca.debian.org/ squeeze-updates main non-free contrib
-deb-src http://ftp.ca.debian.org/ squeeze-updates main non-free contrib
+deb http://ftp.ca.debian.org/ wheezy-updates main non-free contrib
+deb-src http://ftp.ca.debian.org/ wheezy-updates main non-free contrib
 EOF
 aptitude update
 aptitude -y safe-upgrade
@@ -54,6 +54,8 @@ cat > /etc/postfix/main.cf <<'__EOF__'
 append_dot_mydomain = no
 biff = no
 inet_interfaces = loopback-only
+mailbox_command = procmail -a "$EXTENSION"
+mailbox_size_limit = 0
 mydestination = $myhostname, localhost.$mydomain, localhost
 mynetworks_style = host
 myorigin = /etc/mailname
@@ -97,7 +99,7 @@ aptitude install -y \
   git \
   subversion \
   autoconf \
-  automake1.9 \
+  automake \
   bison \
   build-essential \
   debhelper \
@@ -107,21 +109,14 @@ aptitude install -y \
   gdb
 
 #
-# If we are on a 64-bit architecture, then install the 32-bit compatibility libraries.
-#
-if [ "$(uname -m)" = "x86_64" ]; then
-  aptitude install -y ia32-libs ia32-libs-dev libc6-i386 libc6-dev-i386
-fi
-
-#
 # Install a collection of text editors that should satisfy almost everyone.
 #
+aptitude install -y nano
 if [ "${X11}" == "TRUE" ]; then
   aptitude install -y emacs23 vim-gtk
 else
   aptitude install -y emacs23-nox vim
 fi
-aptitude install -y joe nano
 
 #
 # Install Stow
@@ -138,7 +133,7 @@ install --directory --group=staff --mode=2775 --owner=root /usr/local/man
 #
 # Install and enable/configure tmpreaper.
 #
-sed -ie 's/^TMPTIME=.*$/TMPTIME=7/' /etc/default/rcS
+sed -ie 's/^#TMPTIME=.*$/TMPTIME=7/' /etc/default/rcS
 aptitude install -y tmpreaper
 sed -ie 's/^SHOWWARNING/#SHOWWARNING/' /etc/tmpreaper.conf
 
@@ -146,18 +141,16 @@ sed -ie 's/^SHOWWARNING/#SHOWWARNING/' /etc/tmpreaper.conf
 # Install VMWare tools, if we are on a VMWare instance.
 #
 if [ "${VMWARE}" == "TRUE" ]; then
-  aptitude -y install open-vm-source
-  module-assistant -i prepare open-vm
-  module-assistant -i auto-install open-vm
+  aptitude -y install open-vm-dkms
   if [ "${X11}" == "TRUE" ]; then
-    aptitude -u install open-vm-toolbox
+    aptitude -y install open-vm-toolbox
   fi
 fi
 
 #
 # Purge any 'removed' packages.
 #
-dpkg -l | awk '$1 == "rc" { print $2 }' | xargs -r aptitude -y purge
+aptitude -y purge '~c'
 
 #
 # Install some utility scripts, and our user environment if necessary.
